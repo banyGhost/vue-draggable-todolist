2a3ca89ac0c4512cbc2314af9f12ef58c5986b21
任梁樊 <654985930@qq.com>
2018-12-19 13:08:05 +0800

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,9 @@
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    useGrouping: true
+})
+
+console.log(formatter.format(undefined))
